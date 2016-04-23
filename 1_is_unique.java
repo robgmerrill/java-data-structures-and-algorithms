8e2b9ec80d6d540dbb734e526f20e56930adc23c
Rob Merrill <robgmerrill@gmail.com>
2016-04-23 09:21:41 -0700

    first commit

---
+++ 1_is_unique.java
@@ -0,0 +1,17 @@
+// Is Unique: Implement an algorithm to determine if a 
+// string has all unique characters. What if you cannot 
+// use additional structures?
+
+boolean isUniqueChars(String str) {
+  if (str.length() > 128) return false;
+
+  boolean[] char_set = new boolean[128];
+  for (int i = 0; i < str.length(); i++) {
+    int val = str.charAt(i);
+    if (char_set[val]) {
+      return false;
+    }
+    char_set[val] = true;
+  }
+  return true;
+}
---
+++ 2_check_permutation.java
@@ -0,0 +1,15 @@
+// Check Permutation: Given two strings, write a method 
+// to decide if one is a permutation of the other.
+
+String sort(String s) {
+  char[] content = s.toCharArray();
+  java.util.Arrays.sort(content);
+  return new String(content);
+}
+
+boolean permutation(String s, String t) {
+  if (s.length() != t.length()) {
+    return false;
+  }
+  return sort(s).equals(sort(t));
+}
